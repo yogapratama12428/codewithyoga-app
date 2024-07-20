1fd0efcb1e351d6ac3726fe9d07cbf97215b6ada
Yoga Pratama Pangestu <64063549+yogapratama12428@users.noreply.github.com>
2024-07-20 17:37:49 +0700

    Version 1.4.4.2
    + modify button

--- app/(dashboard)/_components/mobile-sidebar.tsx
+++ app/(dashboard)/_components/mobile-sidebar.tsx
@@ -1,10 +1,22 @@
-import { icons, Menu } from "lucide-react";
+import { icons, LayoutDashboard, Menu, Minimize2 } from "lucide-react";
 
 import { 
     Sheet,
     SheetContent,
     SheetTrigger,
   } from "@/components/ui/sheet";
+
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
+
   
 import { Sidebar } from "./Sidebar";
 import { Logo } from "./logo";
@@ -18,8 +30,8 @@ export const MobileSidebar = () => {
       <div className="block md:hidden">
         <Sheet> 
           <SheetTrigger asChild>
-            <Button size="icon" variant="ghost">
-              <Menu className="h-4 w-4"/>
+            <Button size="icon">
+              <Menu className="h-6 w-6"/>
             </Button>
           </SheetTrigger>
           <SheetContent side="left" className="p-0 bg-white">
@@ -27,6 +39,35 @@ export const MobileSidebar = () => {
           </SheetContent>
         </Sheet>
       </div>
+      {/* <div className="block md:hidden">
+      <Drawer>
+        <DrawerTrigger asChild>
+           <Button size="icon">
+              <Menu className="h-6 w-6"/>
+            </Button>
+        </DrawerTrigger>
+        <DrawerContent>
+          <div className="mx-auto w-full h-96">
+            <div className="flex flex-wrap gap-2 mx-2 ">
+
+              <Button asChild>
+                <div className="border m-4 bg-sky-950 flex flex-col gap-2 w-20 h-20">
+                  <LayoutDashboard className="w-7 h-7"/>
+                    <p className=" text-xs text-white">
+                        Dashboard
+                    </p>
+                </div>
+              </Button>
+                
+
+            </div>
+
+
+          </div>
+        </DrawerContent>
+      </Drawer>
+
+      </div> */}
       <div className="hidden md:block">
         <Logo />
       </div> 
